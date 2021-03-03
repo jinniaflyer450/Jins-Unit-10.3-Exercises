05d6b74c4faabade9ad3bb026e971795c77b1b29
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-03-03 12:55:10 -0600

    finished assignment

---
+++ arrow_functions_exercise.js
@@ -0,0 +1,32 @@
+//Original function:
+
+//function double(arr) {
+//    return arr.map(function(val) {
+//      return val * 2;
+//    });
+//  }
+
+//New function:
+
+let double = arr => arr.map(val => val * 2)
+
+
+//Original function:
+
+//function squareAndFindEvens(numbers){
+//    var squares = numbers.map(function(num){
+//      return num ** 2;
+//    });
+//    var evens = squares.filter(function(square){
+//      return square % 2 === 0;
+//    });
+//    return evens;
+//  }
+
+  //New function:
+
+  const squareAndFindEvens = nums => {
+    const squares = nums.map(num => num**2)
+    const evens = squares.filter(square => square%2 === 0)
+    return evens
+  }
